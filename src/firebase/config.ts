5954c01d1f9f74e0962ac2b11f84b049385c4388
carlos cassano <cassano9703@gmail.com>
2025-10-22 20:04:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5698097440-ab57f",
+  "appId": "1:688222159231:web:200851d24a9ab143e99a4a",
+  "apiKey": "AIzaSyDzW3EXLqhfq9ZLLeRaexrWQ5D1H3fgQvQ",
+  "authDomain": "studio-5698097440-ab57f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "688222159231"
+};
