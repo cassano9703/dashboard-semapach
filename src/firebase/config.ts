d0005f3d469534cb1f24e2a7b9789ef2652c1876
carlos cassano <cassano9703@gmail.com>
2025-10-24 17:38:08 +0000

    alguna otro servidor gratuito con el que pueda conectarlo ?

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-5698097440-ab57f",
-  "appId": "1:688222159231:web:200851d24a9ab143e99a4a",
-  "apiKey": "AIzaSyDzW3EXLqhfq9ZLLeRaexrWQ5D1H3fgQvQ",
-  "authDomain": "studio-5698097440-ab57f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "688222159231"
+  projectId: "studio-2e242",
+  appId: "1:1012876694619:web:1e37389569766324670591",
+  apiKey: "api-key-is-a-secret",
+  authDomain: "studio-2e242.firebaseapp.com",
+  storageBucket: "studio-2e242.appspot.com",
+  messagingSenderId: "1012876694619",
+  measurementId: "G-149V4V31G0",
 };
