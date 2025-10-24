6ef2ae2bcd1eb08d897214fc59f0adbeec7aa6de
carlos cassano <cassano9703@gmail.com>
2025-10-24 19:41:26 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-2e242",
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:1012876694619:web:1e37389569766324670591",
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "api-key-is-a-secret",
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-2e242.firebaseapp.com",
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-2e242.appspot.com",
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "1012876694619",
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "G-149V4V31G0",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDzW3EXLqhfq9ZLLeRaexrWQ5D1H3fgQvQ",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-5698097440-ab57f.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-5698097440-ab57f",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-5698097440-ab57f.firebasestorage.app",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "688222159231",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:688222159231:web:200851d24a9ab143e99a4a",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
