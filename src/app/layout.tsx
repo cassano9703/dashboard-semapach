6c9f7f27f9ba0cef80bf4af4c9a83977a8016cec
carlos cassano <cassano9703@gmail.com>
2025-10-24 21:37:33 +0000

    puedo optimizar la pagina para que sea mas rapida?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ import {
 import {MainNav} from '@/components/layout/main-nav';
 import {UserNav} from '@/components/layout/user-nav';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
 export const metadata: Metadata = {
   title: 'Panel Estadístico SEMAPACH',
@@ -24,18 +27,8 @@ export default function RootLayout({
   return (
     <html lang="es" suppressHydrationWarning>
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
       </head>
-      <body className="font-body antialiased">
+      <body className={`${inter.variable} font-body antialiased`}>
         <FirebaseClientProvider>
           <Toaster />
           <SidebarProvider>
