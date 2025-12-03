1db46e3395ca4d8d03a9b52ec251ae8e52ac3aeb
carlos cassano <cassano9703@gmail.com>
2025-12-03 15:41:54 +0000

    ahora si, nos enfocaremos netamente en este modulo en el cual realizarem

--- src/app/compendio/reporte-cobranza/page.tsx
+++ src/app/compendio/reporte-cobranza/page.tsx
@@ -4,7 +4,7 @@ export default function ReporteCobranzaPage() {
   return (
     <div className="flex flex-col gap-6">
       <h1 className="text-3xl font-bold tracking-tight">
-        Reporte de Oficina de Cobranza
+        Oficina de Cobranza 2025
       </h1>
       <CutsAndReconnectionsReport />
     </div>
