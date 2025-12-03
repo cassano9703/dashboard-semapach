ba0725b452508e4e3bf6b21f563de8f17e0932aa
carlos cassano <cassano9703@gmail.com>
2025-12-03 16:59:09 +0000

    retroceder un cambio

--- src/app/compendio/reporte-cobranza/page.tsx
+++ src/app/compendio/reporte-cobranza/page.tsx
@@ -1,7 +1,23 @@
 'use client';
 
+import { AnnualCollectionGoal } from '@/components/compendio/annual-collection-goal';
 import { MonthlyAchievementsGallery } from '@/components/compendio/monthly-achievements-gallery';
+import { MonthlyCollectionGoal } from '@/components/compendio/monthly-collection-goal';
+import { Separator } from '@/components/ui/separator';
 
 export default function ReporteCobranzaPage() {
-  return <MonthlyAchievementsGallery />;
+  return (
+    <div className="flex flex-col gap-8">
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+        <div className="lg:col-span-2">
+          <MonthlyAchievementsGallery />
+        </div>
+        <div className="lg:col-span-1 flex flex-col gap-8">
+          <AnnualCollectionGoal />
+        </div>
+      </div>
+      <Separator />
+      <MonthlyCollectionGoal />
+    </div>
+  );
 }
