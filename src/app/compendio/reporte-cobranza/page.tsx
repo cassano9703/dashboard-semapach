c7cb11f8ba284899be43a9c743550b865862c9bd
carlos cassano <cassano9703@gmail.com>
2025-12-03 16:58:32 +0000

    solo dejar Galeria de Logros

--- src/app/compendio/reporte-cobranza/page.tsx
+++ src/app/compendio/reporte-cobranza/page.tsx
@@ -1,23 +1,7 @@
 'use client';
-import { AnnualCollectionGoal } from '@/components/compendio/annual-collection-goal';
+
 import { MonthlyAchievementsGallery } from '@/components/compendio/monthly-achievements-gallery';
-import { MonthlyCollectionGoal } from '@/components/compendio/monthly-collection-goal';
 
 export default function ReporteCobranzaPage() {
-  return (
-    <div className="flex flex-col gap-8">
-      <h1 className="text-4xl font-bold tracking-tight text-center font-headline">
-        Oficina de Cobranza 2025
-      </h1>
-      <AnnualCollectionGoal />
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
-        <div className="lg:col-span-5">
-          <MonthlyAchievementsGallery />
-        </div>
-        <div className="lg:col-span-7">
-          <MonthlyCollectionGoal />
-        </div>
-      </div>
-    </div>
-  );
+  return <MonthlyAchievementsGallery />;
 }
