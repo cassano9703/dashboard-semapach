109477e86406db63a1aeb5d60cde17194c718745
carlos cassano <cassano9703@gmail.com>
2025-12-03 17:57:57 +0000

    PARA QUE VAYA DE ACUERDO CON EL COLOR, COLOQUEMOSLE DE FONDO UN CELESTE

--- src/app/compendio/layout.tsx
+++ src/app/compendio/layout.tsx
@@ -3,5 +3,5 @@ export default function CompendioLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="bg-[#F9F8F4] p-4 rounded-lg min-h-full">{children}</div>;
+  return <div className="bg-gradient-to-br from-blue-50 via-white to-cyan-50 p-4 rounded-lg min-h-full">{children}</div>;
 }
