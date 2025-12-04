8b1d8d53788f85d4e04867481e7f60a2b735b17b
carlos cassano <cassano9703@gmail.com>
2025-12-04 15:07:47 +0000

    perfecto pero deseo que vaya debajo de galeria

--- src/app/compendio/reporte-cobranza/page.tsx
+++ src/app/compendio/reporte-cobranza/page.tsx
@@ -21,11 +21,11 @@ export default function ReporteCobranzaPage() {
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
         <div className='lg:col-span-5 flex flex-col gap-8'>
             <MonthlyAchievementsGallery />
+            <ServiceOperationStats />
         </div>
         <div className='lg:col-span-7 flex flex-col gap-8'>
             <MonthlyCollectionGoal />
             <DistrictProgressChart />
-            <ServiceOperationStats />
         </div>
       </div>
     </div>
